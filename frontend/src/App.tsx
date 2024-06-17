2e4f2d0539b4e8d56a4b716671453f5ed4c9763a
Doyun <materokatti@gmail.com>
2024-06-17 20:23:47 +0900

    feature : set frontend with React

---
+++ frontend/src/App.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {GoogleLogin} from "react-google-login";
+
+const App: React.FC = () => {
+  const handleLogin = (googleData: any) => {
+    fetch("http://localhost:3000/auth/google/callback", {
+      method: "POST",
+      body: JSON.stringify({
+        token: googleData.tokenId,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // 여기서 받은 데이터로 작업 수행
+        console.log(data);
+      });
+  };
+
+  return (
+    <div className='App'>
+      <h1>Google Login</h1>
+      <GoogleLogin
+        clientId='YOUR_GOOGLE_CLIENT_ID'
+        buttonText='Log in with Google'
+        onSuccess={handleLogin}
+        onFailure={handleLogin}
+        cookiePolicy={"single_host_origin"}
+      />
+    </div>
+  );
+};
+
+export default App;
